3750e699ae88bc1acd7904485bcab08127c8939a
VIBHU SAHU <77403072+VibhuSahu@users.noreply.github.com>
2025-06-12 14:50:50 +0000

    done for now

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
     public static void main(String[] args) {
         
-        System.err.println(singleFiboNumber(6));
+        System.err.println(singleFiboNumber(0));
 
 
         
